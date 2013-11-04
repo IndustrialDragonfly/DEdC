14fd758aad27f5f37e4c95848e086e28af603449
Eugene Davis <eugene_davis@industrialdragonfly.com>
2013-11-03 20:12:14 -0600

    Wrote User Model

---
+++ UserModel/User.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * The User class models regular users, without admin rights.
+ *
+ * @author Eugene Davis
+ */
+class User
+{
+    //<editor-fold desc="Attributes" defaultstate="collapsed">
+    private $Name;
+    private $ID;
+    private $Organization;
+
+    //</editor-fold>
+    //<editor-fold desc="Constructor" defaultstate="collapsed">
+    /**
+     * Pass the contructor the username and organization name to create
+     * a regular User object.
+     * @param String $Name
+     * @param String $Org
+     */
+    public function __construct($Name, $Org)
+    {
+        $this->Name = $Name;
+        $this->Organization = $Org;
+        // Generate random ID
+        $this->ID = bin2hex(openssl_random_pseudo_bytes(127));
+    }
+
+    //</editor-fold>
+    //<editor-fold desc="Accessor functions" defaultstate="collapsed">
+    /**
+     * Sets the name of the user
+     * @param String $newName
+     */
+    public function setName($newName)
+    {
+        $this->Name = $newName;
+    }
+
+    /**
+     * Returns the user name
+     * @return String
+     */
+    public function getName()
+    {
+        return $this->Name;
+    }
+
+    /**
+     * Returns the randomly generated ID string
+     * @return String
+     */
+    public function getId()
+    {
+        return $this->ID;
+    }
+
+    /**
+     * Sets a new organization for the user
+     * @param String $newOrg
+     */
+    public function setOrganization($newOrg)
+    {
+        $this->Organization = $newOrg;
+    }
+
+    /**
+     * Returns the string of the organization name
+     * @return String
+     */
+    public function getOrganization()
+    {
+        return $this->Organization;
+    }
+    /**
+     * Returns whether the user is an admin. For a regular user, is hardcoded
+     * to return false.
+     * @return boolean
+     */
+    public function isAdmin()
+    {
+        return FALSE;
+    }
+//</editor-fold>
+}
+?>
