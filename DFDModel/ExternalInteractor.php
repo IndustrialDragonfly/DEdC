7c2b2a74535238f668f1a6d55b09094d6a5bf77a
Eugene Davis <eugene_davis@industrialdragonfly.com>
2013-11-13 17:10:52 -0600

    Improved database access speeds by moving prepare statement above loops where possible.

--- DFDModel/ExternalInteractor.php
+++ DFDModel/ExternalInteractor.php
@@ -1,6 +1,8 @@
 <?php
+
 require_once 'Node.php';
 require_once 'Constants.php';
+
 /**
  * Description of ExternalInteractor
  *
@@ -9,68 +11,65 @@ require_once 'Constants.php';
 class ExternalInteractor extends Node
 {
 
-   //<editor-fold desc="Attributes" defaultstate="collapsed">
-   //</editor-fold>
-   
-   //<editor-fold desc="Constructor" defaultstate="collapsed">
-   public function __construct()
-   {
-      parent::__construct();
-   }
+    //<editor-fold desc="Attributes" defaultstate="collapsed">
+    //</editor-fold>
+    //<editor-fold desc="Constructor" defaultstate="collapsed">
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-   //</editor-fold>
-   
-   //<editor-fold desc="Accessor functions" defaultstate="collapsed">
-   //</editor-fold>
-   
-   //<editor-fold desc="DB functions" defaultstate="collapsed">
-   /**
-    * function that will save this object to the database
-    * @param PDO $pdo this is the connection to the Database
-    */
-   public function save($pdo)
-   {
-      //<editor-fold desc="save to Entity table" defaultstate="collapsed">
-      // Prepare the statement
-      $insert_stmt = $pdo->prepare("INSERT INTO entity (id, label, type, originator) VALUES(?,?,?,?)");
+    //</editor-fold>
+    //<editor-fold desc="Accessor functions" defaultstate="collapsed">
+    //</editor-fold>
+    //<editor-fold desc="DB functions" defaultstate="collapsed">
+    /**
+     * function that will save this object to the database
+     * @param PDO $pdo this is the connection to the Database
+     */
+    public function save($pdo)
+    {
+        //<editor-fold desc="save to Entity table" defaultstate="collapsed">
+        // Prepare the statement
+        $insert_stmt = $pdo->prepare("INSERT INTO entity (id, label, type, originator) VALUES(?,?,?,?)");
 
-      // Bind the parameters of the prepared statement
-      $type = Constants::ExternalInteractor;
-      $insert_stmt->bindParam(1, $this->id);      
-      $insert_stmt->bindParam(2, $this->label);
-      $insert_stmt->bindParam(3, $type);
-      $insert_stmt->bindParam(4, $this->originator);
+        // Bind the parameters of the prepared statement
+        $type = Constants::ExternalInteractor;
+        $insert_stmt->bindParam(1, $this->id);
+        $insert_stmt->bindParam(2, $this->label);
+        $insert_stmt->bindParam(3, $type);
+        $insert_stmt->bindParam(4, $this->originator);
 
-      // Execute, catch any errors resulting
-      $insert_stmt->execute();
-      //</editor-fold>
-      
-      //<editor-fold desc="save to Element table" defaultstate="collapsed">
-      // Prepare the statement
-      $insert_stmt = $pdo->prepare("INSERT INTO element (id, x, y) VALUES(?,?,?)");
+        // Execute, catch any errors resulting
+        $insert_stmt->execute();
+        //</editor-fold>
+        //<editor-fold desc="save to Element table" defaultstate="collapsed">
+        // Prepare the statement
+        $insert_stmt = $pdo->prepare("INSERT INTO element (id, x, y) VALUES(?,?,?)");
 
-      // Bind the parameters of the prepared statement
-      $insert_stmt->bindParam(1, $this->id);      
-      $insert_stmt->bindParam(2, $this->x);
-      $insert_stmt->bindParam(3, $this->y);
+        // Bind the parameters of the prepared statement
+        $insert_stmt->bindParam(1, $this->id);
+        $insert_stmt->bindParam(2, $this->x);
+        $insert_stmt->bindParam(3, $this->y);
 
-      // Execute, catch any errors resulting
-      $insert_stmt->execute();
-      //</editor-fold>
-      
-      //<editor-fold desc="save to Node table" defaultstate="collapsed">
-      // Prepare the statement
-      for ($i = 0; $i < $this->getNumberOfLinks(); $i++)
-      {
-         $insert_stmt = $pdo->prepare("INSERT INTO node (id, df_id) VALUES(?,?)");
-         // Bind the parameters of the prepared statement
-         $insert_stmt->bindParam(1, $this->id);      
-         $insert_stmt->bindParam(2, $this->links[$i]->getId());
-         // Execute, catch any errors resulting
-         $insert_stmt->execute();
-      }
-      //</editor-fold>
-   }
-   //</editor-fold>
+        // Execute, catch any errors resulting
+        $insert_stmt->execute();
+        //</editor-fold>
+        //<editor-fold desc="save to Node table" defaultstate="collapsed">
+        // Prepare the statement
+        $insert_stmt = $pdo->prepare("INSERT INTO node (id, df_id) VALUES(?,?)");
+        for ($i = 0; $i < $this->getNumberOfLinks(); $i++)
+        {
+            // Bind the parameters of the prepared statement
+            $insert_stmt->bindParam(1, $this->id);
+            $insert_stmt->bindParam(2, $this->links[$i]->getId());
+            // Execute, catch any errors resulting
+            $insert_stmt->execute();
+        }
+        //</editor-fold>
+    }
+
+    //</editor-fold>
 }
+
 ?>
