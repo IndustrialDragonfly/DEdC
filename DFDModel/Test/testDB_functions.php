a0c3c2502b27ca0c04d89af773bb5f86d7e3e54d
Eugene Davis <eugene_davis@industrialdragonfly.com>
2013-11-13 12:51:33 -0600

    Ported DB scripts to Postgres. Modified test files to support Postgres fully.

--- DFDModel/Test/testDB_functions.php
+++ DFDModel/Test/testDB_functions.php
@@ -1,56 +1,78 @@
 <?php
+
 /**
  * Description of testDB_functions
  *
  * @author Josh Clark
  */
+
 class testDB_functions
-{
-   public function getConnection()
-   {
-      $db_hostname = 'localhost';
-      $db_database = 'dedc';
-      $db_username = 'tester';
-      $db_password = 'test';
+{ 
+    // Comment and uncomment the relevant ones for your prefered RDMS
+    // Probably should be improved with an enum
+    protected static $db_type = "mysql";
+    //protected static $db_type = "postgres";
+    public function getConnection()
+    {
+        $db_hostname = 'localhost';
+        $db_database = 'dedc';
+        $db_username = 'tester';
+        $db_password = 'test';
 
-      // Combined driver/host/db string
-      // Comment and uncomment the relevant ones for your prefered RDMS
-      $db_id = "mysql:host=$db_hostname;dbname=$db_database";
-      //$db_id = "pgsql:host=$db_hostname;dbname=$db_database";
+        if ("mysql" == self::$db_type)
+        {
+            $db_id = "mysql:host=$db_hostname;dbname=$db_database";
+        }
+        if ("postgres" == self::$db_type)
+        {
+            $db_id = "pgsql:host=$db_hostname;dbname=$db_database";
+        }
 
-      // DB Setup
-      $dbh;
-      try 
-      {
-          $dbh = new PDO($db_id, $db_username, $db_password);
-      }
-      catch (PDOException $e)
-      {
-          die("Failed to connect to DB" . $e->getMessage());
-      }
-      $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-      return $dbh;
-   }
-   
-   public function resetDB($pdo)
-   {
-      if( $pdo instanceof PDO)
-      {
-         $pdo->query('USE dedc;');
-         $pdo->query('SET foreign_key_checks=0;');
-         $pdo->query('TRUNCATE TABLE entity;');
-         $pdo->query('TRUNCATE TABLE element;');
-         $pdo->query('TRUNCATE TABLE dataflow;');
-         $pdo->query('TRUNCATE TABLE node;');
-         $pdo->query('TRUNCATE TABLE external_links;');
-         $pdo->query('TRUNCATE TABLE element_list;');
-         $pdo->query('TRUNCATE TABLE multiprocess;');
-         $pdo->query('SET foreign_key_checks=1;');
-      }
-      else 
-      {
-         throw new BadFunctionCallException("input parameter was not a PDO");
-      }
-   }
+        // DB Setup
+        $dbh;
+        try
+        {
+            $dbh = new PDO($db_id, $db_username, $db_password);
+        } catch (PDOException $e)
+        {
+            die("Failed to connect to DB" . $e->getMessage());
+        }
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $dbh;
+    }
+
+    public function resetDB($pdo)
+    {
+        if ($pdo instanceof PDO)
+        {
+            $pdo->query('BEGIN');           
+            $pdo->query('TRUNCATE TABLE element;');
+            $pdo->query('TRUNCATE TABLE dataflow;');
+            $pdo->query('TRUNCATE TABLE node;');
+            $pdo->query('TRUNCATE TABLE external_links;');
+            $pdo->query('TRUNCATE TABLE element_list;');
+            $pdo->query('TRUNCATE TABLE multiprocess;');
+            
+            // Truncate the foreign key restricted entity table in mysql
+            if ("mysql" == self::$db_type)
+            {
+                $pdo->query('SET foreign_key_checks=0;');
+                $pdo->query('TRUNCATE TABLE entity;');
+                $pdo->query('SET foreign_key_checks=1;');
+            }
+            
+            // Truncate the foreign key restricted table in postgres
+            if ("postgres" == self::$db_type)
+            {
+               $pdo->query('TRUNCATE TABLE entity CASCADE;'); 
+            }
+            $pdo->query('COMMIT');
+        } else
+        {
+            throw new BadFunctionCallException("input parameter was not a PDO");
+        }
+    }
+
 }
+
 ?>
