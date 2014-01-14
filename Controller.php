120a1c92cb1e651f634f8beda12f71d70994011f
Eugene Davis <eugene@eugenemdavis.net>
2014-01-14 17:18:51 -0600

    Summary - Added redirect to the web client and made web client work from there
    Controller.php: Check if text/html is an Accept type and assume that is
    a browser, if so, redirect to the Javascript client
    Frontend/dist/index.html: Replaced with index.php
    Frontend/dist/index.php: Replaces index.html, allows the web client to
    be loaded from the client

--- Controller.php
+++ Controller.php
@@ -9,6 +9,19 @@
  * discussion on its design. 
  */
 
+/*
+ * Checks to see if the incoming request has the proper user agent product,
+ * if it does not loads the web client page.
+ */
+$web_client_location = "Frontend/dist/";
+$browser_accept = '*/*';
+if (FALSE !== stripos($_SERVER['HTTP_ACCEPT'], $browser_accept))
+{
+    require_once $web_client_location."/index.php";
+    webClient($web_client_location);
+    exit;
+}
+
 /**
  * Autoload function which loads a file based on class name from any of the 
  * relevant folders - those of the data models and those of Request and Response
