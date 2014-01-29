80aab0e425485a88af57ec035753b0d8028a017d
Jacob Swanson <jacob_swanson@industrialdragonfly.com>
2014-01-29 11:05:41 -0600

    Summary - Moved Connector code into DEdC module

--- Frontend/dev/canvas/src/canvas.js
+++ Frontend/dev/canvas/src/canvas.js
@@ -168,11 +168,11 @@ Raphael.st.draggable = function(callback, element)
 			$(deleteButton).button().click(function(){
 					getCurrentCanvas().removeElementFromSelection();
 			});
-			//var connector = new Connector(canvas);
 
 			// Load DFD button
 			$(load).button().click(function(){
-				//connector.load("Controller.php/test_dfd");
+				// Controller.php is required until the rewrite rules work correctly
+				Connector.get("Controller.php/test_dfd");
 			});
 
 			// New tab button
@@ -926,7 +926,104 @@ Raphael.st.draggable = function(callback, element)
 		};
 	}
 
+	/**
+	 * Base Response for requests
+	 * @constructor
+	 */
+	var Response = function()
+	{
+		var me = this;
+		var myStatus = null;
+		var myData = null;
+		var myError = null;
 
+		/**
+		 * Get the status of the Response
+		 * @return {String} Status given from jQuery
+		 */
+		this.getStatus = function()
+		{
+			return myStatus;
+		};
 
+		/**
+		 * Set the status of the Response
+		 * @param {String} Status
+		 * @returns {Response} this, for chaining
+		 */
+		this.setStatus = function(status)
+		{
+			myStatus = status;
+			return me;
+		};
+
+		/**
+		 * Get the data returned from the server
+		 * @returns {String} Data as string from server
+		 */
+		this.getData = function()
+		{
+			return myData;
+		};
+
+		/**
+		 * Set the status of the Response
+		 * @param {String} data Data
+		 * @returns {Response} this, for chaining
+		 */
+		this.setData = function(data)
+		{
+			myData = data;
+			return me;
+		};
+
+		/**
+		 * Get the error message if there was an error
+		 * @returns {String} Error message
+		 */
+		this.getError = function()
+		{
+			return myError;
+		};
+
+		/**
+		 * Set the error message
+		 * @param {String} error Error message
+		 * @returns {Response} this, for chaining
+		 */
+		this.setError = function(error)
+		{
+			myError = error;
+			return me;
+		};
+	};
+
+	var Connector = (function() {
+
+		var publicGet = function(url, successCallback, failCallback) {
+			var response = new Response();
+			
+			$.ajax({
+				accepts: "application/json",
+				url: url,
+				async: false,		// If async is true, response will be returned before query executes
+				dataType: "json"	// Do not let jQuery automatically parse the JSON response
+			}).done(function(data, textStatus) {
+					// Request was successful
+					response.setData(data);
+					response.setStatus(textStatus);
+			}).fail(function(jqXHR,textStatus,errorThrown) {
+					// Request failed for some reason
+					response.setStatus(textStatus);
+					response.setError("GET " + url + " " + jqXHR.status + " (" + jqXHR.statusText + ")");
+			});
+		}
+
+		return {
+			get: publicGet
+		}
+	})();
+
+	// Expose the DEdC module
 	window.DEdC = DEdC;
 })();
