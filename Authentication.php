5e0d270b7940b971c64befc2272f5a5893ece1bf
Eugene Davis <eugene@eugenemdavis.net>
2014-03-14 12:23:11 -0500

    Summary - Added experiment with HTTP Basic Auth to Controller
    Authentication.php: Request that user sends a response using HTTP Basic
    Auth, and check it against a hardcoded user/password of
    Malcolm/IndustrialDragonfly
    Controller.php: Moved redirection to page below authentication and
    authorization check.

--- Authentication.php
+++ Authentication.php
@@ -5,6 +5,31 @@
  */
    function authenticateUser()
    {
-       return TRUE;
+        if (!isset($_SERVER['PHP_AUTH_USER'])) 
+        {
+            header('WWW-Authenticate: Basic realm="My Realm"');
+            // If user hits cancel, goes to this next line
+            unauthorized();
+            return false;
+        } 
+        else 
+        {
+            if ($_SERVER['PHP_AUTH_USER'] == "Malcolm" && $_SERVER['PHP_AUTH_PW'] == "IndustrialDragonfly")
+            {
+                return true;
+            }
+            else
+            {
+                unauthorized();
+                return false;
+            }
+        }
+   }
+   
+   
+   function unauthorized()
+   {
+        header('HTTP/1.0 401 Unauthorized');
+        echo 'Unable to log you in.';
    }
 ?>
--- Controller.php
+++ Controller.php
@@ -20,18 +20,6 @@ function existingElementFactory($id, $storage)
     return $element;
 }
 
-/*
- * Checks to see if the incoming request has the proper user agent product,
- * if it does not loads the web client page.
- */
-$web_client_location = "Frontend/";
-$browser_accept = '*/*';
-if (FALSE !== stripos($_SERVER['HTTP_ACCEPT'], $browser_accept))
-{
-    require_once $web_client_location."/index.php";
-    webClient($web_client_location);
-    exit;
-}
 
 /**
  * Autoload function which loads a file based on class name from any of the 
@@ -81,7 +69,6 @@ require_once "AuthorizeUser.php";
         // TODO - handle authentication
         exit;
     }
-    
     // Determine if user has correct permissions to perform the action
     if (!authorizeUser())
     {
@@ -89,6 +76,19 @@ require_once "AuthorizeUser.php";
         // TODO - handle authorization
         exit;
     }
+    
+    /*
+    * Checks to see if the incoming request has the proper user agent product,
+    * if it does not loads the web client page.
+    */
+   $web_client_location = "Frontend/";
+   $browser_accept = '*/*';
+   if (FALSE !== stripos($_SERVER['HTTP_ACCEPT'], $browser_accept))
+   {
+       require_once $web_client_location."/index.php";
+       webClient($web_client_location);
+       exit;
+   }
     
     // Retrieve information about request and put it in a request object
     $body = file_get_contents('php://input'); // Get the body of the request
