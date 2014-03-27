5ededa49008af9f49f5b40edbc85e9fa9af9157f
Eugene Davis <eugene@eugenemdavis.net>
2014-03-27 15:16:41 -0500

    Summary - Added Jeb, our second user, to the database script

--- db_scripts/mysql_db_create.sql
+++ db_scripts/mysql_db_create.sql
@@ -137,6 +137,12 @@ CREATE USER 'tester'@'localhost' IDENTIFIED BY 'test';
 GRANT ALL ON dedc.* TO 'tester'@'localhost';
 
 #Insert a user
-INSERT INTO users (id, userName, organization) VALUES ('RlyxJ3ZxPsdOr9rFTb9UrTrGKRBUQxWbclf9Gv0Fz1Mx', 'Geoff', 'InD');
-#password is password1
-INSERT INTO hash (id, hash) VALUES ('RlyxJ3ZxPsdOr9rFTb9UrTrGKRBUQxWbclf9Gv0Fz1Mx ', '$2y$10$RMwCqfIrwxTmnzQoEw8B4ePQcH3TmVIQ35xQcTbltOdtXbPvT.3pi');
+INSERT INTO users (id, userName, organization) VALUES ('RlyxJ3ZxPsdOr9rFTb9UrTrGKRBUQxWbclf9Gv0Fz1Mx', 'Geoff', 'InD')
+;
+INSERT INTO users (id, userName, organization) VALUES ('3LToOJ5L0gRLVRcYDThHYxne49Ai13xKHwVJx6Y4Ixox', 'Jeb', 'InD');
+
+#password for Geoff is password1
+INSERT INTO hash (id, hash) VALUES ('RlyxJ3ZxPsdOr9rFTb9UrTrGKRBUQxWbclf9Gv0Fz1Mx', '$2y$10$RMwCqfIrwxTmnzQoEw8B4ePQcH3TmVIQ35xQcTbltOdtXbPvT.3pi');
+
+#password for Bob is password2
+INSERT INTO hash (id, hash) VALUES ('3LToOJ5L0gRLVRcYDThHYxne49Ai13xKHwVJx6Y4Ixox', '$2y$10$VVK5rcQAz/wd59ckep16Quu8T/vJ7ExXRmW6Ya3lVdc2xnwQV6xUS');
